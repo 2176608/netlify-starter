5ab77d22d59bb4623c4fe63e535b5282005fbba3
2176608 <90504717+2176608@users.noreply.github.com>
2025-07-24 00:04:56 +0800

    更新 comment.js
    再次更改格式；由双引号改为单引号

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -5,8 +5,8 @@ const serverless = require('serverless-http');
 const app = Waline({
   env: 'netlify',
   async postSave(comment) {
-//设置关键词过滤
-forbiddenWords: ["习近平", "毛泽东", "邓小平", "胡锦涛", "江泽民", "垃圾", "傻逼", "SB", "Sb", "sB", "sb",],
+    //设置关键词过滤
+    forbiddenWords: ['习近平', '毛泽东', '邓小平', '胡锦涛', '江泽民', '垃圾', '傻逼', 'SB', 'Sb', 'sB', 'sb',],
   },
 });
 
