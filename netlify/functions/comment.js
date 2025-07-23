65582f83858cd72fcb761ab6254cfee00dbe7f02
2176608 <90504717+2176608@users.noreply.github.com>
2025-07-23 23:36:28 +0800

    更新 comment.js
    添加关键词过滤

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -5,7 +5,8 @@ const serverless = require('serverless-http');
 const app = Waline({
   env: 'netlify',
   async postSave(comment) {
-    // do what ever you want after save comment
+  //设置关键词过滤
+  forbiddenWords: ["习近平", "毛泽东", "邓小平", "胡锦涛", "江泽民", "垃圾", "傻逼", "SB", "Sb", "sB", "sb",],
   },
 });
 
