fdbbb8549c62d920eaca3e1c3aaa97889d98adca
2176608 <90504717+2176608@users.noreply.github.com>
2025-07-24 13:00:08 +0800

    更新 comment.js

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -4,9 +4,7 @@ const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
-  async postSave(comment) {
-    //设置关键词过滤
-    forbiddenWords: ['习近平', '毛泽东', '邓小平', '胡锦涛', '江泽民', '垃圾', '傻逼', 'SB', 'Sb', 'sB', 'sb',],
+  async postSave(comment) {//设置关键词过滤forbiddenWords: ['习近平', '毛泽东', '邓小平', '胡锦涛', '江泽民', '垃圾', '傻逼', 'SB', 'Sb', 'sB', 'sb',],
   },
 });
 
