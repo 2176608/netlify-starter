141cb051cc57f9f22fdaa0a5d8ca20859579e1f8
2176608 <90504717+2176608@users.noreply.github.com>
2025-07-24 13:01:58 +0800

    更新 comment.js

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -4,8 +4,7 @@ const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
-  async postSave(comment){ forbiddenWords: ['习近平', '毛泽东', '邓小平', '胡锦涛', '江泽民', '垃圾', '傻逼', 'SB', 'Sb', 'sB', 'sb',],
-  },
+  async postSave(comment)forbiddenWords: ['习近平', '毛泽东', '邓小平', '胡锦涛', '江泽民', '垃圾', '傻逼', 'SB', 'Sb', 'sB', 'sb',],
 });
 
 module.exports.handler = serverless(http.createServer(app));
